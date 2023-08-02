e97424ecf93ec5399bcb47910aa18eab5e8956e2
Joao Gloria <jhfgloria@gmail.com>
2023-08-02 22:17:25 +0100

    one-off module

---
+++ src/email/email.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class EmailService {
+  async send(email: string) {
+    console.log(`Sending email to ${email}`);
+  }
+}
--- src/one-off.module.ts
+++ src/one-off.module.ts
@@ -1,4 +1,23 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { SendEmailToAdmins } from './one-offs';
+import { EmailModule } from './email/email.module';
 
-@Module({})
+@Module({
+  imports: [
+    EmailModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      logging: false,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+    }),
+  ],
+  providers: [SendEmailToAdmins],
+  exports: [SendEmailToAdmins],
+})
 export class OneOffModule {}
